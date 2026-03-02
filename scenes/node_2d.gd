b186196cd1601dc4fb4142d08994b403ded1aa85
gkdjldgs <jermaise@gmail.com>
2026-03-01 19:03:44 -0700

    ye

--- scenes/node_2d.gd
+++ scenes/node_2d.gd
@@ -1,6 +1,6 @@
 extends Node2D
 @onready var text1 = $Part1/RichTextLabel
-
+@onready var sound = $AudioStreamPlayer2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,3 +12,7 @@ func _process(delta: float) -> void:
 		text1.show()
 	if autoload.tutorial == 2:
 		text1.hide()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	sound.play()
